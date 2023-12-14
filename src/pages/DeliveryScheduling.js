c6537978af476a150b24997b6f3e159c61977cc2
Md yasen rafe <mdyasenrafe@gmail.com>
2023-12-14 13:49:19 +0600

    delivery page done

--- src/pages/DeliveryScheduling.js
+++ src/pages/DeliveryScheduling.js
@@ -1,11 +1,30 @@
 import { Container } from "@mui/material";
 import React, { useEffect, useState } from "react";
+import dayjs from "dayjs";
+import { DemoContainer, DemoItem } from "@mui/x-date-pickers/internals/demo";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import { Toast } from "../components/common/Toast";
+import { useNavigate } from "react-router-dom";
 
 export default function DeliveryScheduling() {
-  const [selectedDate, setSelectedDate] = useState();
+  const [selectedDate, setSelectedDate] = useState(dayjs());
+  const navigate = useNavigate();
 
   const handleContinue = () => {
-    console.log(selectedDate);
+    if (!selectedDate) {
+      Toast.fire({
+        icon: "error",
+        title: "Please select a date",
+      });
+      return;
+    }
+    navigate("/image-uploads");
+  };
+
+  const handleDateChange = (newValue) => {
+    setSelectedDate(newValue);
   };
 
   return (
@@ -16,18 +35,23 @@ export default function DeliveryScheduling() {
             Choose a Delivery Date
           </h1>
         </div>
-        <div className="m-5">
-          <div className="my-10">
-            <input
-              type="date"
-              value={new Date(selectedDate)}
-              onChange={(date) => setSelectedDate(date)}
-              className="border border-gray-300 rounded-lg w-full h-[40px] md:h-[48px] px-4"
-              placeholder="Select a date"
-            />
-          </div>
+        <div className="m-5 mb-5">
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DemoContainer
+              components={[
+                "DatePicker",
+                "MobileDatePicker",
+                "DesktopDatePicker",
+                "StaticDatePicker",
+              ]}
+            >
+              <DemoItem label="Choose a date">
+                <DatePicker value={selectedDate} onChange={handleDateChange} />
+              </DemoItem>
+            </DemoContainer>
+          </LocalizationProvider>
           <button
-            className="h-[40px] md:h-[48px] w-full bg-[#3c7fff] hover:bg-indigo-800 rounded-lg text-white flex justify-center items-center"
+            className="h-[40px] md:h-[48px] w-full bg-[#3c7fff] hover:bg-indigo-800 rounded-lg text-white flex justify-center items-center mt-5"
             type="submit"
             onClick={handleContinue}
           >
